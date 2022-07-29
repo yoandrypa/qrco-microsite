280031fd53ca17851d716674db64fc7ce17f39d2
alainfd82 <alainfd82@gmail.com>
2022-07-29 18:34:18 -0400

    - hotfix/include tls en redis client

--- server/redis.ts
+++ server/redis.ts
@@ -6,7 +6,8 @@ import env from "./env";
 const client = redis.createClient({
   host: env.REACT_REDIS_HOST,
   port: env.REACT_REDIS_PORT,
-  ...(env.REACT_REDIS_PASSWORD && { password: env.REACT_REDIS_PASSWORD })
+  ...(env.REACT_REDIS_PASSWORD && { password: env.REACT_REDIS_PASSWORD }),
+  tls: {}
 });
 
 export const get: (key: string) => Promise<any> = promisify(client.get).bind(
