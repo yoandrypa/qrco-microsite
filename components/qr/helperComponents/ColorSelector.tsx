79d1b023343fd2c6657c7411514c397c96b80457
soma <soma@velosimo.com>
2022-08-27 17:45:17 -0400

    making color selector fully available

--- components/qr/helperComponents/ColorSelector.tsx
+++ components/qr/helperComponents/ColorSelector.tsx
@@ -1,11 +1,15 @@
 // @ts-nocheck
 
+'use strict'
+
 import { useEffect, useRef, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
+import Box from '@mui/material/Box';
+import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
 
-import { ColorPicker } from '@programmerraj/rc-color-picker';
+import { SketchPicker } from 'react-color';
 
 const hex = /^#?([0-9A-Fa-f]{3}){1,2}$/;
 
@@ -22,12 +26,18 @@ interface ColorSelProps {
   label: string;
   property: string;
   handleData: Function;
-};
+}
 
 const ColorSelector = ({ color, handleData, label, property }: ColorSelProps) => {
+  const [anchor, setAnchor] = useState(null);
   const [value, setValue] = useState(color || '#000000');
+
   const ref = useRef(null);
   const cursorPos = useRef<number>(0);
+
+  const handlePicker = ({ currentTarget }) => {
+    setAnchor(currentTarget);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     cursorPos.current = event.target.selectionStart || 0;
@@ -39,6 +49,10 @@ const ColorSelector = ({ color, handleData, label, property }: ColorSelProps) =>
     if (text.length && hex.test(value)) {
       setValue(handleValue(text));
     }
+  };
+
+  const handleColor = payload => {
+    handleData(property)(payload.hex);
   };
 
   useEffect(() => {
@@ -62,36 +76,58 @@ const ColorSelector = ({ color, handleData, label, property }: ColorSelProps) =>
   }, [value]);
 
   return (
-    <TextField
-      size="small"
-      fullWidth
-      margin="dense"
-      variant="outlined"
-      label={label}
-      value={value.slice(1)}
-      onChange={handleChange}
-      onPaste={handlePaste}
-      inputProps={{ ref }}
-      error={!hex.test(value)}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Typography>#</Typography>
-          </InputAdornment>
-        ),
-        endAdornment: (
-          <InputAdornment position="end" sx={{ mt: '5px', mr: '-5px' }}>
-            <ColorPicker
-              animated
-              color={color}
-              onChange={handleData(property)}
-              mode="RGB"
-              enableAlpha={false}
-            />
-          </InputAdornment>
-        )
-      }}
-    />
+    <>
+      <TextField
+        size="small"
+        fullWidth
+        margin="dense"
+        variant="outlined"
+        label={label}
+        value={value.slice(1)}
+        onChange={handleChange}
+        onPaste={handlePaste}
+        inputProps={{ ref }}
+        error={!hex.test(value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Typography>#</Typography>
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end" sx={{ mt: '5px', mr: '-5px' }}>
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                  p: '3px',
+                  mt: '-5px',
+                  mr: '-3px',
+                  width: '28px',
+                  height: '28px',
+                  borderRadius: '5px',
+                  border: 'solid 1px black',
+                  borderColor: theme => theme.palette.text.disabled,
+                  backgroundClip: 'content-box',
+                  backgroundColor: color || 'inherit'
+                }}
+                onClick={handlePicker} />
+            </InputAdornment>
+          )
+        }}
+      />
+      {anchor && (
+        <Popover
+          id="reasonPopover"
+          open
+          anchorEl={anchor}
+          onClose={() => { setAnchor(null); }}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <SketchPicker color={color} onChangeComplete={handleColor} disableAlpha presetColors={[]}/>
+        </Popover>
+      )}
+    </>
   );
 };
 
