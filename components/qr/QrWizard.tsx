d54da627b70a4e160aae73f1a65a5c46ddb413dc
soma <soma@velosimo.com>
2022-09-06 18:13:26 -0400

    uploading

--- components/qr/QrWizard.tsx
+++ components/qr/QrWizard.tsx
@@ -1,4 +1,4 @@
-import {ReactNode, useContext} from 'react';
+import {ReactNode, useContext, useState} from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -7,7 +7,6 @@ import Button from '@mui/material/Button';
 import Context from "../context/Context";
 import {styled} from "@mui/material/styles";
 import * as linkHandler from "../../handlers/links";
-import {QR_CONTENT_ROUTE} from "./constants";
 
 const steps = ['Type of QR', 'QR content', 'QR design'];
 
@@ -26,6 +25,8 @@ const QrWizard = ({children}: QrWizardProps) => {
   // @ts-ignore
   const { selected, step, setStep, data, userInfo }: StepsProps = useContext(Context);
 
+  const [loading, setLoading] = useState<boolean>(false);
+
   const handleBack = () => {
     setStep((prev: number) => prev - 1);
   };
@@ -34,16 +35,22 @@ const QrWizard = ({children}: QrWizardProps) => {
     try {
       return await linkHandler.create({
         body: { target: "https://wwww.ebanux.com", reuse: false  },
-        user: {id: userInfo.attributes.sub}
+        user: { id: userInfo.attributes.sub }
       });
     } catch (error) {
-      return {error}
+      return { error }
     }
   }
 
   const handleNext = async () => {
     if (step === 1 && selected === 'vcard+' && data?.isDynamic) {
+      setLoading(true);
       const value = await handleShort();
+      setLoading(false);
+      // @ts-ignore
+      if (!value.error) {
+
+      }
     } else {
       setStep((prev: number) => prev + 1);
     }
@@ -51,11 +58,16 @@ const QrWizard = ({children}: QrWizardProps) => {
 
   return (
     <>
+      {loading && (
+        <Box sx={{ position: 'fixed', top: '10px', left: '10px' }}>
+          Fetching data. Please wait...
+        </Box>
+      )}
       <Box sx={{ minHeight: 'calc(100vh - 220px)' }}>
         {children}
       </Box>
       <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', pt: 2 }}>
-        <StepperButtons variant="contained" disabled={step === 0 || !Boolean(selected)} onClick={handleBack}>
+        <StepperButtons variant="contained" disabled={loading || step === 0 || !Boolean(selected)} onClick={handleBack}>
           Back
         </StepperButtons>
         <Stepper activeStep={step} alternativeLabel sx={{ width: '100%' }}>
@@ -66,7 +78,7 @@ const QrWizard = ({children}: QrWizardProps) => {
           ))}
         </Stepper>
         {/* @ts-ignore */}
-        <StepperButtons onClick={handleNext} disabled={step === 2 || !Boolean(selected)} variant="contained">
+        <StepperButtons onClick={handleNext} disabled={loading || step === 2 || !Boolean(selected)} variant="contained">
           Next
         </StepperButtons>
       </Box>
