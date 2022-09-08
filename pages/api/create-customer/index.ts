955f3ec964926e9b03ec7bdad8bac7e95a869655
yosle <yosle007@gmail.com>
2022-09-08 08:31:13 -0400

    api testing

--- pages/api/create-customer/index.ts
+++ pages/api/create-customer/index.ts
@@ -1,11 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
-
-<<<<<<< HEAD
 import {User} from '../../../models/user'
 
-=======
->>>>>>> 06742ef (create customer endpoint)
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: '2022-08-01',
@@ -17,32 +14,23 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   ) {
 
     if (req.method === 'POST') {
-<<<<<<< HEAD
 try {
   const users = User
 
- const results =  await users.all().exec((error: any, results: any) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log("results are ", results);
+ const results =  await users.all() ;
+        console.log("results are ", results.st);
         // [ Document { name: 'Will', breed: 'Terrier', id: 1 },
         //   lastKey: undefined,
         //   count: 1,
         //   queriedCount: 2,
         //   timesQueried: 1 ]
-        console.log(results[0]); // { name: 'Will', breed: 'Terrier', id: 1 }
+     
         console.log(results.count); // 1
         console.log(Array.isArray(results)); // true
-        console.log(results.scannedCount); // 2
-    }
-});
-  console.log(users.toJSON())
+    
 } catch (error) {
-  
+  console.log("Error ", error)
 }
-=======
->>>>>>> 06742ef (create customer endpoint)
 
         try {
             //TODO validate params
