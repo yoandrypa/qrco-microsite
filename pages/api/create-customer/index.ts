2ae23060a6539b852b32e47c3669de2d621437e3
yosle <yosle007@gmail.com>
2022-09-09 13:45:09 -0400

    checkout create and customer create

--- pages/api/create-customer/index.ts
+++ pages/api/create-customer/index.ts
@@ -1,12 +1,47 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
-import {User} from '../../../models/user'
-
-
+import {update, find} from '../../../handlers/users'
+import {PLAN_TEST_MODE_PRICES,} from '../../../consts'
+//init stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: '2022-08-01',
   })
+
+
+  async function createCustomerInStripe(email: string) {
+  try {
+    const params: Stripe.CustomerCreateParams = {
+      email: email
+    }
+    const customer: Stripe.Customer = await stripe.customers.create(params)
+    return customer.id
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : 'Internal server error'
+    return err as Error
+  }
+}
+
+async function createCheckoutSession(
+  customer_id: string,
+   plan_type: string) {
+  try {
+     const session = stripe.checkout.sessions.create({
+      mode: 'subscription',
+      line_items: [
+        {
+          price: '',
+          // For metered billing, do not pass quantity
+          quantity: 1
+        }
+      ],
+      success_url: `${DOMAIN}/plans/account?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${DOMAIN}/plans/`
+     })
+  } catch (error) {
+    
+  }
+}
 
   export default async function handler(
     req: NextApiRequest,
@@ -14,41 +49,35 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   ) {
 
     if (req.method === 'POST') {
-try {
-  const users = User
-
- const results =  await users.all() ;
-        console.log("results are ", results.st);
-        // [ Document { name: 'Will', breed: 'Terrier', id: 1 },
-        //   lastKey: undefined,
-        //   count: 1,
-        //   queriedCount: 2,
-        //   timesQueried: 1 ]
+      if (!req.body.id || !req.body.email || !req.body.plan_type){
+        return res.status(400).send('Missing parameters in request (email, plan_type and id)')
+      } 
      
-        console.log(results.count); // 1
-        console.log(Array.isArray(results)); // true
-    
-} catch (error) {
-  console.log("Error ", error)
-}
+      const userData = await find(req.body.id)  
 
-        try {
-            //TODO validate params
-            const params: Stripe.CustomerCreateParams = {
-                email: req.body.email
+      if(!userData){
+        return res.status(404).send('No user found for this id ' + req.body.id )
+      }
+
+      console.log('user data ',JSON.stringify(userData))  
+         //creating new customer 
+          if (!userData.plan_customer_id){ 
+            const customer_id = await createCustomerInStripe(req.body.email);
+            if (customer_id instanceof Error ){
+               return res.status(500).json({error: true, message: customer_id.message})
             }
-            const customer: Stripe.Customer =
-              await stripe.customers.create(params)
-      
-            res.status(200).json(customer)
-          } catch (err) {
-            const errorMessage =
-              err instanceof Error ? err.message : 'Internal server error'
-            res.status(500).json({ statusCode: 500, message: errorMessage })
+           const updateResult = await update({id: req.body.id}, {plan_customer_id: customer_id})
+            if(!updateResult){
+              return res.status(500).json({message: 'Could not save customer in database'})
+            }
           }
 
-    } else {
+          res.status(200)
+      } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method Not Allowed')
   }
+
+
+
   }
