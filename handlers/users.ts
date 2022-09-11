2022e1d05049e9952e3c08a3de75f40005366413
yosle <yosle007@gmail.com>
2022-09-11 13:58:29 -0400

    deleteSusbcription handler

--- handlers/users.ts
+++ handlers/users.ts
@@ -21,3 +21,7 @@ return updatedUser
 export const findByCustomerId = async (customerId: string): Promise<UserType> => {
   return await query.user.findByCustomerId({ customerId: { eq: customerId } })
 };
+
+export const deleteUserSubscription = async (customerId: Match<UserType>) =>{
+  return await query.user.deleteSubscription({customerId: customerId})
+} 
