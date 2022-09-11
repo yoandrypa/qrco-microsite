42c67ded571eb7b038d196469dc31de8f98313c5
yosle <yosle007@gmail.com>
2022-09-11 12:30:10 -0400

    onCheckoutSessionCompleted event handler

--- handlers/webhooks.ts
+++ handlers/webhooks.ts
@@ -1,46 +1,67 @@
 
 import Stripe from 'stripe'
-import {update as updateUserInDB} from '../handlers/users'
+import {update as updateUserInDB, find} from '../handlers/users'
+import { findByCustomerId as findUserByCustomerId } from '../handlers/users';
+
+
+function buildUserSubscription(
+  subscription: Stripe.Subscription,
+  status: Stripe.Checkout.Session.PaymentStatus
+): UserSubscription {
+  const lineItem = subscription.items.data[0];
+  const price = lineItem.price;
+
+  return {
+    id: subscription.id,
+    priceId: price?.id,
+    status,
+    currency: lineItem.price.currency ?? null,
+    interval: price?.recurring?.interval ?? null,
+    intervalCount: price?.recurring?.interval_count ?? null,
+    createdAt: subscription.created,
+    periodStartsAt: subscription.current_period_start,
+    periodEndsAt: subscription.current_period_end,
+    trialStartsAt: subscription.trial_start ?? null,
+    trialEndsAt: subscription.trial_end ?? null,
+  };
+}
+
+async function setUserSubscription(
+    customerId: string,
+    subscription: UserSubscription,
+  ) {
+   const {id} = await findUserByCustomerId(customerId) 
+       
+  try {
+   await updateUserInDB({id: id},{subscriptionData: subscription})      
+  } catch (error) {
+    console.log('error saving user data')
+  }
+}
 
 export async function onCheckoutCompleted(
     session: Stripe.Checkout.Session,
     subscription: Stripe.Subscription
   ) {
-
     const customerId = session.customer as string;    
     // status can either be paid ,unpaid or no_payment_required
     const status = session.payment_status;
   
     const subscriptionData 
-      = buildUserSubscription(subscription, status);
-    
+      = buildUserSubscription(subscription, status);    
     // use your DB methods to 
-    // set organization.subscription=subscriptionData
-  
-    return setUserSubscription({
+    // set user.subscription=subscriptionData  
+    return setUserSubscription(
       customerId,
-      subscription: subscriptionData,
-    });
+      subscriptionData,
+    );
   }
 
-  function buildUserSubscription(
-    subscription: Stripe.Subscription,
-    status: Stripe.Checkout.Session.PaymentStatus
-  ): UserSubscription {
-    const lineItem = subscription.items.data[0];
-    const price = lineItem.price;
-  
-    return {
-      id: subscription.id,
-      priceId: price?.id,
-      status,
-      currency: lineItem.price.currency ?? null,
-      interval: price?.recurring?.interval ?? null,
-      intervalCount: price?.recurring?.interval_count ?? null,
-      createdAt: subscription.created,
-      periodStartsAt: subscription.current_period_start,
-      periodEndsAt: subscription.current_period_end,
-      trialStartsAt: subscription.trial_start ?? null,
-      trialEndsAt: subscription.trial_end ?? null,
-    };
+  export async function deleteUserSubscription(customerId){
+    return setUserSubscription(
+      customerId,
+      '',
+    );
+
   }
+
