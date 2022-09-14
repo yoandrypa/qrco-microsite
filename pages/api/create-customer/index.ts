c52c5f6532ed1dc6de294ca7f5dcc0bf35351da9
yosle <yosle007@gmail.com>
2022-09-14 10:21:46 -0400

    handling exceptions

--- pages/api/create-customer/index.ts
+++ pages/api/create-customer/index.ts
@@ -105,11 +105,16 @@ async function createCheckoutSession(
           }
            
           //create checkout session
-      const session = await createCheckoutSession({ customer_id: userData.plan_customer_id, plan_type: req.body.plan_type }) 
-      if (session instanceof Error ) {
-        return res.status(500).json({error: true, message: session.message})
-      } 
-        res.status(200).json({result: session})
+          try {
+            const session = await createCheckoutSession({ customer_id: userData.plan_customer_id, plan_type: req.body.plan_type }) 
+            res.status(200).json({result: session})
+          } catch (error) {
+            if (error instanceof Error ) {
+              return res.status(500).json({error: true, message: session.message})
+            } 
+            
+          }
+     
   } else {
     //Incorrect method
     res.setHeader('Allow', 'POST')
