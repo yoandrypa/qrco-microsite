f44158fc0c5ddff1cd1f0a5b5f70cdc88e07558f
alainfd82 <alainfd82@gmail.com>
2022-09-15 06:55:09 -0400

    - Updated the views of static and dynamic QRs available. - Included logic to upload files to Amazon S3. - Created the view to upload assets: pdfs, images, audios and videos

--- components/qr/QrWizard.tsx
+++ components/qr/QrWizard.tsx
@@ -8,8 +8,8 @@ import Context from "../context/Context";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import DoneIcon from "@mui/icons-material/Done";
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import { styled } from "@mui/material/styles";
 
 import { useRouter } from "next/router";
@@ -17,11 +17,11 @@ import { useRouter } from "next/router";
 import * as linkHandler from "../../handlers/links";
 import { generateShortLink } from "../../utils";
 import * as QrHandler from "../../handlers/qrs";
-import {BackgroundType, CornersAndDotsType, DataType, FramesType, OptionsType} from "./types/types";
-import {createMainDesign, updateDesign} from "../../handlers/qrDesign";
-import {QR_TYPE_ROUTE} from "./constants";
-import {areEquals} from "../helpers/generalFunctions";
-import initialData, {initialBackground, initialFrame} from "../../helpers/qr/data";
+import { BackgroundType, CornersAndDotsType, DataType, FramesType, OptionsType } from "./types/types";
+import { createMainDesign, updateDesign } from "../../handlers/qrDesign";
+import { QR_TYPE_ROUTE } from "./constants";
+import { areEquals } from "../helpers/generalFunctions";
+import initialData, { initialBackground, initialFrame } from "../../helpers/qr/data";
 
 const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -56,8 +56,10 @@ const QrWizard = ({ children }: QrWizardProps) => {
   const [isError, setIsError] = useState<boolean>(false);
 
   // @ts-ignore
-  const { selected, step, setStep, data, setData, userInfo, options, setOptions, frame, background, cornersData,
-    dotsData, isWrong, loading, setLoading }: StepsProps = useContext(Context);
+  const {
+    selected, step, setStep, data, setData, userInfo, options, setOptions, frame, background, cornersData,
+    dotsData, isWrong, loading, setLoading
+  }: StepsProps = useContext(Context);
 
   const router = useRouter();
 
@@ -80,8 +82,8 @@ const QrWizard = ({ children }: QrWizardProps) => {
   const handleNext = async () => {
     // @ts-ignore
     if (step === 0 && Boolean(data.isDynamic) && !Boolean(userInfo)) {
-      router.push({ pathname: "/", query: { path: router.pathname, login: true } }, "/");
-    } else if (step === 1 && Boolean(userInfo) && ['vcard+', 'web'].includes(selected)) {
+      await router.push({ pathname: "/", query: { path: router.pathname, login: true } }, "/");
+    } else if (step === 1 && Boolean(userInfo) && ["vcard+", "web", "pdf", "image", "audio", "video"].includes(selected)) {
       setLoading(true);
       // @ts-ignore
       const model = { ...data, qrType: selected, userId: userInfo.attributes.sub };
@@ -92,7 +94,7 @@ const QrWizard = ({ children }: QrWizardProps) => {
           // @ts-ignore
           model.qrOptionsId = design;
           // @ts-ignore
-          setOptions({...options, id: design.id});
+          setOptions({ ...options, id: design.id });
         }
 
         let qr;
@@ -101,10 +103,10 @@ const QrWizard = ({ children }: QrWizardProps) => {
           qr = await QrHandler.create(model);
         } else {
           // @ts-ignore
-          qr = await QrHandler.edit({...data, id: data.id});
+          qr = await QrHandler.edit({ ...data, id: data.id });
         }
 
-        setData({...data, id: qr.id});
+        setData({ ...data, id: qr.id });
 
         let shortLink;
         if (data?.isDynamic) {
@@ -112,14 +114,16 @@ const QrWizard = ({ children }: QrWizardProps) => {
           const targetUrl = generateShortLink("qr/" + qr.id);
           shortLink = await handleShort(targetUrl);
           // @ts-ignore
-          await QrHandler.edit({id: qr.id, userId: userInfo.attributes.sub, shortLinkId: shortLink.id});
+          await QrHandler.edit({ id: qr.id, userId: userInfo.attributes.sub, shortLinkId: shortLink.id });
         }
 
         setLoading(false);
         // @ts-ignore
         if (!shortLink?.error) {
           // @ts-ignore
-          if (shortLink?.link) { setOptions({ ...options, data: shortLink?.link }); }
+          if (shortLink?.link) {
+            setOptions({ ...options, data: shortLink?.link });
+          }
           setStep(2);
         }
       } catch {
@@ -138,20 +142,24 @@ const QrWizard = ({ children }: QrWizardProps) => {
           if (!Boolean(updater)) {
             updater = { ...options };
           }
-        }
+        };
 
         const areTheSame = () => {
           const cleaner = (obj: OptionsType) => {
             const o = JSON.parse(JSON.stringify(obj));
             // @ts-ignore
-            if (o.data !== undefined) { delete o.data; }
+            if (o.data !== undefined) {
+              delete o.data;
+            }
             // @ts-ignore
-            if (o.qrOptions.typeNumber !== undefined) { delete o.qrOptions.typeNumber; }
+            if (o.qrOptions.typeNumber !== undefined) {
+              delete o.qrOptions.typeNumber;
+            }
             return o;
-          }
+          };
 
           return areEquals(cleaner(options), cleaner(initialData));
-        }
+        };
 
         if (!areTheSame) {
           initializeUpdater();
@@ -181,16 +189,16 @@ const QrWizard = ({ children }: QrWizardProps) => {
         try {
           // const result = await updateDesign(idDesignRef || '', updater);
           if (updater !== undefined) {
-            await updateDesign(options.id || '', updater);
+            await updateDesign(options.id || "", updater);
           }
-          router.push({pathname: '/', query: { clear: true }}, '/', { shallow: true });
+          await router.push({ pathname: "/", query: { clear: true } }, "/", { shallow: true });
         } catch {
           setIsError(true);
         }
         setLoading(false);
       }
       if (!Boolean(userInfo)) {
-        router.push(QR_TYPE_ROUTE, undefined, { shallow: true });
+        await router.push(QR_TYPE_ROUTE, undefined, { shallow: true });
       }
     } else {
       setStep((prev: number) => prev + 1);
@@ -226,11 +234,12 @@ const QrWizard = ({ children }: QrWizardProps) => {
             (step === 1 && Boolean(userInfo) && !Boolean(data?.qrName?.trim().length))
           }
           variant="contained">
-          {step >= 2 ? 'Last' : 'Next'}
+          {step >= 2 ? "Last" : "Next"}
         </StepperButtons>
       </Box>
       {isError && (
-        <Snackbar open autoHideDuration={3500} onClose={() => setIsError(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Snackbar open autoHideDuration={3500} onClose={() => setIsError(false)}
+                  anchorOrigin={{ vertical: "top", horizontal: "center" }}>
           <Alert onClose={() => setIsError(false)} severity="error">
             Error accessing data.
           </Alert>
