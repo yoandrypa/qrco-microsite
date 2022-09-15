b3dc9b9f984b9a977a696ddd223e5625ab485454
yosle <yosle007@gmail.com>
2022-09-15 15:09:09 -0400

    using gitignore for test dir

--- pages/api/testing/index.ts
+++ pages/api/testing/index.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {update, findByCustomerId} from '../../../handlers/users'
 
 type ResponseData = {
   name: string
 }
 
-export default function handler(
+export default async function  handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+ const {id} = await findByCustomerId("123")
+ console.log(id)
+ try {
+  const query = await update({id: id},{subscriptionData:{
+    createdDate: 2323,
+    currency:'usd',
+    id:'sdjlsjdas',
+    interval:'day',
+    intervalCount: 1,
+    periodEndsAt: 2314,
+    periodStartsAt: 23134,
+    priceId:'sfdsad23e3',
+    status:'sAS',
+    trialEndsAt: 343432,
+    trialStartsAt: 3123
+  }})
+
+ console.log(query)
+ } catch (error) {
+  res.status(500).json(error as ResponseData)
+ }
+
+
 }
