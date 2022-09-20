ea7c11b7b47fe0ede65b9159fe0cbbd41964b2a3
Alain Fernández Deroncere <alainfd82@gmail.com>
2022-09-20 00:02:54 -0400

    - Added functionality to upload files to AWS S3 in separate folders. - The disablement of the delete button in the QR list was included while the validation is finished. - A modification was made in the message that appears when there are no QRs created (still in progress)

--- queries/storage.ts
+++ queries/storage.ts
@@ -1,19 +1,26 @@
 import { CustomError } from "../utils";
 import { s3Client } from "../libs";
+import S3 from "aws-sdk/clients/s3";
 
-export const upload = async (file: File) => {
-  const uploadParams = {
-    Bucket: process.env.REACT_AWS_BUCKET_NAME,
+export const upload = async (file: File, path: string = "") => {
+  const uploadParams: S3.PutObjectRequest = {
+    Bucket: String(process.env.REACT_AWS_BUCKET_NAME),
     Body: await file.text(),
-    Key: "/QrLink/assets/" + file.name,
+    Key: path + "/" + file.name,
     ContentLength: file.size,
     ContentType: file.type
   };
-  // @ts-ignore
-  return s3Client.upload(uploadParams, (err, data) => {
-    /*if (err) {
-      throw new CustomError("Error uploading QR", 500, err);
-    }*/
-    return data;
+  return s3Client.upload(uploadParams).promise().then(res => {
+    return res;
+  }).catch(err => {
+    throw new CustomError("Error uploading file", 500, err);
   });
+};
+
+export const download = (key: string) => {
+  //TODO
+};
+
+export const remove = (key: string) => {
+  //TODO
 };
