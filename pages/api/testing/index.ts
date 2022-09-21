bbdd82cd6d678436c94074503dc1ba4a395c9eae
yosle <yosle007@gmail.com>
2022-09-21 19:18:13 -0400

    testing subscription endpoint

--- pages/api/testing/index.ts
+++ pages/api/testing/index.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {update, find,findByCustomerId} from '../../../handlers/users'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.REACT_STRIPE_SECRET_KEY || 'sk_test_51Ksb3LCHh3XhfaZr2tgzaQKAQtuTF9vRtgdXBS7X2rAaPC6FNoLQ3hyPFVmlnRhsif0FDdbi5cdgEh7Y1Wt9Umo900w9YPUGo6', {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: '2022-08-01',
+})
 
 
 type ResponseData = {
@@ -12,8 +18,10 @@ export default async function  handler(
   res: NextApiResponse<ResponseData>
 ) {
  if (req.method == 'POST'){
-  const customer = req.body.customer as string 
-  const id = await findByCustomerId(customer)
+   
+   const subscription = await stripe.subscriptions.retrieve(req.body.subscription);
+   const customer = subscription.customer 
+  const {id} = await findByCustomerId(customer as string)
   return res.status(200).json({id})  
  } else {
   return res.status(400)
