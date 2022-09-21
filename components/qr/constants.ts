1b1ac59ea2850776a4996dc4293461efd0c68d84
soma <soma@velosimo.com>
2022-09-20 22:40:37 -0400

    these changes are meant to: - fix web regular expression - qrlist

--- components/qr/constants.ts
+++ components/qr/constants.ts
@@ -8,5 +8,10 @@ export const DYNAMIC_QR = ['twitter', 'whatsapp', 'facebook', 'web'] as string[]
 export const SOCIALS = ['facebook', 'whatsapp', 'twitter', 'instagram', 'linkedin', 'pinterest', 'telegram', 'youtube'] as string[];
 export const PARAM_QR_TEXT = 'qr_text' as string;
 
-export const WEB = new RegExp('^((https?)://)?[\\w]+[\\-\\w:%\\.\\+~#=]{1,256}\\.(?:[\\-\\w()@:%_+.~#?&/=]*)$');
+export const WEB = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+  '(\\#[-a-z\\d_]*)?$','i');
 export const EMAIL = new RegExp('^\\w+(\\.\\w+)*(\\+\\w+(\\.\\w+)*)?@\\w+(\\.\\w+)+$');
