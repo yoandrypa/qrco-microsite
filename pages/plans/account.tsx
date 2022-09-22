892a8378e7edecebced0c7213362d77e0795339f
yosle <yosle007@gmail.com>
2022-09-22 06:00:02 -0400

    account page added

--- pages/plans/account.tsx
+++ pages/plans/account.tsx
@@ -1,12 +1,22 @@
 
 
 import React from 'react'
-
+import Paper from '@mui/material/Paper'
+import { useRouter } from "next/router";
+import Typography from "@mui/material/Typography"
 type Props = {}
 
  const AccountPage = (props: Props) => {
+  const router = useRouter();
+  const id = router.query["session_id"];
+ console.log(id)
   return (
-    <div>account</div>
+    <>
+    <Typography variant='h4'>
+      Account details
+    </Typography>
+    
+    </>
   )
 }
 
