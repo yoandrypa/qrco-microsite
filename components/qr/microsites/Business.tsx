8f790d73783b26f7ae4eed38d0f305f6f693ea82
soma <soma@velosimo.com>
2022-09-23 18:07:05 -0400

    starting microsite for business

---
+++ components/qr/microsites/Business.tsx
@@ -0,0 +1,97 @@
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import RenderIcon from "../helperComponents/RenderIcon";
+import IconButton from "@mui/material/IconButton";
+import ForwardIcon from "@mui/icons-material/Forward";
+import CardContent from "@mui/material/CardContent";
+import MainMicrosite from "./MainMicrosite";
+
+interface BusinessProps {
+  newData: any;
+}
+
+export default function Business({newData}: BusinessProps) {
+  const renderSocials = (item: string, label: string) => {
+    let value = newData[item] as string;
+    value = value.slice(value.indexOf(':') + 1);
+
+    let url = '' as string;
+    switch (item) {
+      case 'facebook': {
+        url = 'https://www.facebook.com/';
+        break;
+      }
+      case 'twitter': {
+        url = 'https://twitter.com/';
+        break;
+      }
+      case 'pinterest': {
+        url = 'https://www.pinterest.com/';
+        break;
+      }
+      case 'whatsapp': {
+        url = 'https://wa.me/';
+        break;
+      }
+      case 'telegram': {
+        url = 'https://t.me/';
+        break;
+      }
+      case 'linkedin': {
+        url = 'https://www.linkedin.com/in/';
+        break;
+      }
+      case 'instagram': {
+        url = 'https://www.instagram.com/';
+        break;
+      }
+      case 'youtube': {
+        url = 'https://www.youtube.com/';
+        break;
+      }
+    }
+
+    url += value;
+
+    return (
+      <Grid item xs={12} style={{paddingTop: 0}}>
+        <TextField
+          label={label}
+          size="small"
+          fullWidth
+          margin="dense"
+          // @ts-ignore
+          value={value}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start"><RenderIcon icon={item} enabled/></InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton target="_blank" component="a" href={url}><ForwardIcon/></IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Grid>
+    );
+  }
+
+  return (
+    <MainMicrosite>
+      <CardContent>
+        <Grid container spacing={1}>
+          {newData.facebook && renderSocials('facebook', 'Facebook')}
+          {newData.whatsapp && renderSocials('whatsapp', 'Whatsapp')}
+          {newData.twitter && renderSocials('twitter', 'Twitter')}
+          {newData.linkedin && renderSocials('linkedin', 'LinkedIn')}
+          {newData.instagram && renderSocials('instagram', 'Instagram')}
+          {newData.youtube && renderSocials('youtube', 'Youtube')}
+          {newData.pinterest && renderSocials('pinterest', 'Pinterest')}
+          {newData.telegram && renderSocials('telegram', 'Telegram')}
+        </Grid>
+      </CardContent>
+    </MainMicrosite>
+  );
+}
