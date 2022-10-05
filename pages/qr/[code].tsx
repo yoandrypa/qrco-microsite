1a169485e94e72cac26f98e7355b5b95e27da9b8
alainfd82 <alainfd82@gmail.com>
2022-10-05 11:55:00 -0400

    - bugfix/corrected error that did not allow to retrieve the link when you are not logged in.

--- pages/qr/[code].tsx
+++ pages/qr/[code].tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { QrDataModel } from "../../models/qr/QrDataModel";
 
 import VCard from "../../components/qr/microsites/VCard";
@@ -8,7 +8,7 @@ import Coupons from "../../components/qr/microsites/Coupons";
 import SocialInfo from "../../components/qr/microsites/SocialInfo";
 
 // @ts-ignore
-export default function Handler({ data }) {
+export default function Handler({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   if (data === "NO DATA") {
     return <>{"Ops! Unable to process your request."}</>;
   }
@@ -43,34 +43,7 @@ export default function Handler({ data }) {
 export const getServerSideProps: GetServerSideProps = async ({ params, req }) => {
   // @ts-ignore
   const { code } = params;
-
-  // "code" is the id
-  const getUserInfo = async (): Promise<{
-    userData: string;
-  } | null> => {
-    try {
-      let userInfo = { userData: "" };
-      for (const [key, value] of Object.entries(req.cookies)) {
-        // @ts-ignore
-        userInfo[key.split(".").pop()] = value;
-      }
-      // @ts-ignore
-      if (!userInfo.userData) {
-        return null;
-      }
-      return userInfo;
-    } catch (e) {
-      return null;
-    }
-  };
-  const userInfo = await getUserInfo();
-
-  if (!userInfo?.userData) {
-    return { props: { data: "NO DATA" } };
-  }
-  const userData = JSON.parse(userInfo.userData as string);
-  const userId = userData.UserAttributes[0].Value;
-  const data = await QrDataModel.get({ id: code, userId });
+  const data = await QrDataModel.get({ id: code });
   if (!data) {
     return { props: { data: "NO DATA" } };
   }
