be19b6c328b8d4549532b60718393bf842114ca2
alainfd82 <alainfd82@gmail.com>
2022-10-06 23:37:22 -0400

    - bugfix/fixed bugs when uploading, downloading and then displaying image to and from AWS S3.

--- queries/storage.ts
+++ queries/storage.ts
@@ -1,15 +1,15 @@
-import { CustomError } from "../utils";
 import { s3Client } from "../libs";
 import S3 from "aws-sdk/clients/s3";
 
 export const upload = async (file: File, path: string = "") => {
   const uploadParams: S3.PutObjectRequest = {
     Bucket: String(process.env.REACT_AWS_BUCKET_NAME),
-    Body: await file.text(),
+    Body: file,
     Key: path + "/" + file.name,
     ContentLength: file.size,
     ContentType: file.type
   };
+  console.debug({ uploadParams });
   return s3Client.upload(uploadParams).promise().then(res => {
     return res;
   }).catch(err => {
