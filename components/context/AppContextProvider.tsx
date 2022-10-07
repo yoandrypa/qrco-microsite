f296d4adcf49f76deb4705dabad25806018c865b
soma <soma@velosimo.com>
2022-10-07 14:31:25 -0400

    removing not needed dep from dependencies list

--- components/context/AppContextProvider.tsx
+++ components/context/AppContextProvider.tsx
@@ -75,7 +75,7 @@ const AppContextProvider = (props: ContextProps) => {
     } else {
       setData({});
     }
-  }, [isUserInfo, data.isDynamic]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [data.isDynamic]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     if (doneInitialRender.current && router.pathname === QR_TYPE_ROUTE) {
