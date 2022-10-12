6cc752b359b12887b935c4b2906811bb55e124e2
soma <soma@velosimo.com>
2022-10-12 13:33:59 -0400

    updating constants

--- components/qr/constants.ts
+++ components/qr/constants.ts
@@ -2,11 +2,8 @@ import { ColorTypes } from "./types/types";
 
 export const QR_TYPE_ROUTE = '/qr/type' as string;
 export const QR_CONTENT_ROUTE = '/qr/content' as string;
-export const QR_DESIGNER_NEW_ROUTE = '/qr/new' as string;
+export const QR_DESIGNER_NEW_ROUTE = '/qr/design' as string;
 
-export const CUSTOM_WIDTH = '780px' as string;
-
-export const DYNAMIC_QR = ['twitter', 'whatsapp', 'facebook', 'web'] as string[];
 export const SOCIALS = ['facebook', 'whatsapp', 'twitter', 'instagram', 'linkedin', 'pinterest', 'telegram', 'youtube'] as string[];
 export const PARAM_QR_TEXT = 'qr_text' as string;
 
