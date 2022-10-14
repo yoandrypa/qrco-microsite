20aa5c4de61fd9c886b83e88e3a51a034bb0c13f
yosle <yosle007@gmail.com>
2022-10-14 16:15:06 -0400

    microsite layout

--- components/qr/microsites/Donations.tsx
+++ components/qr/microsites/Donations.tsx
@@ -8,10 +8,11 @@ import Typography from '@mui/material/Typography'
 import Avatar from '@mui/material/Avatar'
 import Button from "@mui/material/Button";
 import CardActions from "@mui/material/CardActions";
-import { createTheme ,ThemeProvider} from '@mui/material/styles';
-import { Card, CardMedia } from "@mui/material";
-import { RoundedCorner } from "@mui/icons-material";
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import EmojiFoodBeverageIcon from '@mui/icons-material/EmojiFoodBeverage';
+import SvgIcon from '@mui/material/SvgIcon'
+import Box from '@mui/material/Box'
+import { TextField } from "@mui/material";
 interface DonationsProps {
   newData: any;
 }
@@ -26,7 +27,7 @@ export default function DonationsInfo({ newData }: DonationsProps) {
         main: colors.p,
         // dark: will be calculated from palette.primary.main,
         // contrastText: will be calculated to contrast with palette.primary.main
-     
+
       },
       secondary: {
         // light: '#0066ff',
@@ -34,7 +35,7 @@ export default function DonationsInfo({ newData }: DonationsProps) {
         // dark: will be calculated from palette.secondary.main,
         contrastText: colors.s,
       },
-     
+
       // Used by `getContrastText()` to maximize the contrast between
       // the background and the text.
       contrastThreshold: 3,
@@ -49,50 +50,92 @@ export default function DonationsInfo({ newData }: DonationsProps) {
 
   return (
     //TODO
-    <ThemeProvider theme={theme}>
-   <CardMedia> 
-<CardContent>
+      <CardContent>
 
-      <Grid container
-       display='flex'
-      justifyContent="center"
-      alignItems="center"
-      spacing={1}
-      >
-    <Grid item xs={3} sx={{ backgroundColor: colors.s, RoundedCorner: 2}} >
-      <Typography variant='h6' textAlign={'center'} marginTop={2}>Would you like to buy me a coffie?</Typography>
-        <Stack direction="row" sx={{ marginTop: 2, display: 'flex', justifyContent: 'center', alignSelf: 'center' }}>
-          <Avatar
-            alt="Avatar"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-            sx={{ width: 100, height: 100, }}
-          />
-        </Stack>
-        
+        <Grid container
+          display='flex'
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item xs={3} sx={{ RoundedCorner: 2 }} >
+              <Typography variant='h6' textAlign={'center'} marginTop={2}>{newData?.title}</Typography>
+              <Typography variant='h6' textAlign={'center'} marginTop={2}>Would you like to buy me a coffie?</Typography>
+              <Stack direction="row" sx={{ marginTop: 2, display: 'flex', justifyContent: 'center', alignSelf: 'center' }}>
+                <Avatar
+                  alt="Avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                  sx={{ width: 100, height: 100, }}
+                />
+              </Stack>
+
+         
+
         </Grid>
+        <Grid container sx={{ marginTop: 2, display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+          <Typography>
+            {newData?.message}
+          </Typography>
+          <Typography>
+            {newData?.web}
+          </Typography>
+        </Grid>
+        <Grid spacing={1} container sx={{ marginTop: 2, display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+         <Grid item>
+            <SvgIcon>
+              <EmojiFoodBeverageIcon color='primary'/>
+            </SvgIcon> 
+         </Grid>
+         <Grid item>
+           <Typography >
+            x
+           </Typography>
+         </Grid>
+         <Grid item>
+           <Box sx={{borderRadius:45, backgroundColor: 'lightblue', width:35,height:35, display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}} >
+            <Typography textAlign='center' sx={{display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}}>
+              1
+            </Typography>
+           </Box>
+         </Grid>
+         <Grid item>
+           <Box sx={{borderRadius:45, backgroundColor: 'lightblue', width:35,height:35, display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}} >
+            <Typography textAlign='center' sx={{display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}}>
+              3
+            </Typography>
+           </Box>
+         </Grid>
+         <Grid item>
+           <Box sx={{borderRadius:45, backgroundColor: 'lightblue', width:35,height:35, display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}} >
+            <Typography textAlign='center' sx={{display: 'flex', justifyContent: 'center', alignContent: 'center' , margin:'auto'}}>
+              5
+            </Typography>
+           </Box>
+         </Grid> 
+         <Grid item>
+           <TextField
+           sx={{width: 80}}
+           label='Amount'
+           size="small"
+           type='number'
+           placeholder="25"
+           ></TextField>
+         </Grid> 
        
-      </Grid>
-      <Grid container sx={{ marginTop: 2, display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-        <Typography>
-          {newData?.message}
-        </Typography>
-        <Typography>
-          {newData?.web}
-        </Typography>
-      </Grid>
-      <Grid container sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
 
-        <CardActions sx={{ marginTop: 2 }}>
-          
-          <Button color="primary" sx={{backgroundColor: colors.s}}>
-            Donate         
-          </Button>     
+        </Grid>
 
-        </CardActions>
-      </Grid>
+        <Grid container sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+          <CardActions sx={{ marginTop: 2 }}>
+            <Button color="primary" variant="contained" sx={{borderRadius: 2}}>
+              Donate
+            </Button>
 
-    </CardContent>
-    </CardMedia> 
-    </ThemeProvider>
+          </CardActions>
+         
+        </Grid>
+        </Grid>
+      </CardContent>
+
   );
 }
