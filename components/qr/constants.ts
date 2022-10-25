8ab9f979660e8637e5bf818e6fb1f1c6d547a4f5
soma <soma@velosimo.com>
2022-10-25 15:51:41 -0400

    removing not needed constants

--- components/qr/constants.ts
+++ components/qr/constants.ts
@@ -1,15 +1,6 @@
 import { ColorTypes } from "./types/types";
 
-export const QR_TYPE_ROUTE = '/qr/type' as string;
-export const QR_CONTENT_ROUTE = '/qr/content' as string;
-export const QR_DESIGN_ROUTE = '/qr/design' as string;
-
 export const SOCIALS = ['facebook', 'whatsapp', 'twitter', 'instagram', 'linkedin', 'pinterest', 'telegram', 'youtube'] as string[];
-export const PARAM_QR_TEXT = 'qr_text' as string;
-
-export const EMAIL = new RegExp('^\\w+(\\.\\w+)*(\\+\\w+(\\.\\w+)*)?@\\w+(\\.\\w+)+$', 'i');
-export const PHONE = new RegExp('^(\\+\\d{1,3}\\s?)?((\\(\\d{1,3}\\))|\\d{1,3})(\\s|\\-)?(\\d+((\\s|\\-)\\d+)*)$');
-export const ZIP = new RegExp('^\\d{5}(-\\d{4})?$');
 
 export const DAYS = {
   sun: 'Sunday',
