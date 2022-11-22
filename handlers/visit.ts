cd44e5f01e3a02a4564ed9129da9378346198c27
Alain Fernández Deroncere <alainfd82@gmail.com>
2022-11-22 04:22:58 -0500

    - Added logic to create/update the statistics of each visit to a short link.

---
+++ handlers/visit.ts
@@ -0,0 +1,37 @@
+//const useragent = require("useragent");
+const parser = require("ua-parser-js");
+const geoip = require("fast-geoip");
+import URL from "url";
+import { CustomError, removeWww } from "../utils";
+import * as Visit from "../queries/visit";
+
+const browsersList = ["IE", "Firefox", "Chrome", "Opera", "Safari", "Edge"];
+const osList = ["Windows", "Mac OS", "Linux", "Android", "iOS"];
+
+const filterInBrowser = (agent: { browser: { name: string }; }) => (item: string) =>
+  agent.browser.name.toLowerCase().includes(item.toLocaleLowerCase());
+
+const filterInOs = (agent: { os: { name: string; }; }) => (item: string) =>
+  agent.os.name.toLowerCase().includes(item.toLocaleLowerCase());
+
+// @ts-ignore
+export const create = async (data: any) => {
+  try {
+    const agent = parser(data.headers["user-agent"]);
+    const [browser = "Other"] = browsersList.filter(filterInBrowser(agent));
+    const [os = "Other"] = osList.filter(filterInOs(agent));
+    const referrer =
+      data.referrer && removeWww(URL.parse(data.referrer).hostname);
+    const location = data.realIP ? await geoip.lookup(data.realIP) : undefined;
+    const country = location && location.country;
+    await Visit.create({
+      browser: browser.toLowerCase(),
+      country: country || "Unknown",
+      shortLinkId: data.shortLinkId,
+      os: os.toLowerCase().replace(/\s/gi, ""),
+      referrer: (referrer && referrer.replace(/\./gi, "[dot]")) || "Direct",
+    });
+  } catch (e: any) {
+    throw new CustomError(e.message, e.statusCode || 500, e);
+  }
+};
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,9 @@
-export const generateShortLink = (id: string | undefined, customDomain?: string | undefined): string => {
+export const generateShortLink = (
+  id: string | undefined, customDomain?: string | undefined): string => {
   const protocol =
-    process.env.REACT_APP_CUSTOM_DOMAIN_USE_HTTPS === "true" || customDomain ? "https://" : "http://";
+    process.env.REACT_APP_CUSTOM_DOMAIN_USE_HTTPS === "true" || customDomain
+      ? "https://"
+      : "http://";
   const domain = process.env.REACT_APP_DEFAULT_DOMAIN;
   return `${protocol}${customDomain || domain}/${id}`;
 };
@@ -9,10 +12,15 @@ export class CustomError extends Error {
   public statusCode?: number;
   public data?: any;
 
-  public constructor(message: string, statusCode = 500, data?: any) {
+  public constructor (message: string, statusCode = 500, data?: any) {
     super(message);
     this.name = this.constructor.name;
     this.statusCode = statusCode;
     this.data = data;
   }
+};
+
+export const removeWww = (host?: string | null) => {
+  // @ts-ignore
+  return host.replace("www.", "");
 };
