d6eb48484f07421d8ded35f5ef6e35fb39ade2c9
soma <soma@velosimo.com>
2022-12-13 08:26:32 -0500

    adjustments and fixings for preview assets

--- pages/sample/empty.tsx
+++ pages/sample/empty.tsx
@@ -1,24 +1,16 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
 import MainComponent from "../../components/MainComponent";
-import PleaseWait from "../../components/PleaseWait";
 import {useRouter} from "next/router";
 
 export default function Empty() {
-  const [done, setDone] = useState<boolean>(false);
-
   const router = useRouter();
 
   useEffect(() => {
     if (window.top === window) {
       router.push('/sample');
     }
-    setDone(true);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (!done) { // @ts-ignore
-    return <PleaseWait />;
-  }
 
   return <MainComponent newData={{isAnEmptyPreview: true}}/>;
 }
