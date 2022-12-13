5fcb47a09596769f63b2445cddf1b848dcb91f0f
yosle <yosle007@gmail.com>
2022-12-13 17:29:18 -0500

    small fix for donations previews

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -5,7 +5,7 @@ import PetsId from "./qr/microsites/PetsId";
 import Coupons from "./qr/microsites/Coupons";
 import SocialInfo from "./qr/microsites/SocialInfo";
 import Web from "./qr/microsites/Web";
-import {handleDesignerString} from "../helpers/qr/helpers";
+import { handleDesignerString } from "../helpers/qr/helpers";
 import FileMicro from "./qr/microsites/FileMicro";
 import Donations from "./qr/microsites/Donations";
 import LinksMicro from "./qr/microsites/LinksMicro";
@@ -13,14 +13,14 @@ import Box from "@mui/material/Box";
 import EngineeringIcon from "@mui/icons-material/Engineering";
 import Typography from "@mui/material/Typography";
 import SamplesList from "./SamplesList";
-import {useEffect, useRef, useState} from "react";
-import {ASSETS, GALLERY} from "./helpers/generalFunctions";
+import { useEffect, useRef, useState } from "react";
+import { ASSETS, GALLERY } from "./helpers/generalFunctions";
 
 interface MainCompProps {
   newData: any;
 }
 
-export default function MainComponent({newData}: MainCompProps) {
+export default function MainComponent({ newData }: MainCompProps) {
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<any>(newData.isAnEmptyPreview === undefined ? newData : null);
 
@@ -31,7 +31,7 @@ export default function MainComponent({newData}: MainCompProps) {
 
     if (window.top !== window) { // @ts-ignore
       window.top.postMessage( // @ts-ignore
-        JSON.stringify({readyForDuty: true}), process.env.REACT_APP_QRCO_URL
+        JSON.stringify({ readyForDuty: true }), process.env.REACT_APP_QRCO_URL
       );
       iframed.current = true;
     }
@@ -62,8 +62,8 @@ export default function MainComponent({newData}: MainCompProps) {
         left: "50%",
         transform: "translate(-50%, -50%)",
       }}>
-        <Box sx={{display: "flex", flexDirection: "column", width: "100%"}}>
-          <Typography sx={{mx: "auto"}}>{"Loading..."}</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+          <Typography sx={{ mx: "auto" }}>{"Loading..."}</Typography>
           <Typography sx={{
             color: theme => theme.palette.text.disabled,
             mx: "auto",
@@ -74,47 +74,47 @@ export default function MainComponent({newData}: MainCompProps) {
   }
 
   if (data?.samples) {
-    return <SamplesList newData={data.samples}/>;
+    return <SamplesList newData={data.samples} />;
   }
 
   if (data?.qrType === "vcard+") {
-    return <VCard newData={data}/>;
+    return <VCard newData={data} />;
   }
 
   if (GALLERY.includes(data?.qrType)) {
-    return <Images newData={{...data, iframed: iframed.current}}/>;
+    return <Images newData={{ ...data, iframed: iframed.current }} />;
   }
 
   if (data?.qrType === "business") {
-    return <Business newData={data}/>;
+    return <Business newData={data} />;
   }
 
   if (data?.qrType === "coupon") {
-    return <Coupons newData={data}/>;
+    return <Coupons newData={data} />;
   }
 
   if (data?.qrType === "social") {
-    return <SocialInfo newData={data}/>;
+    return <SocialInfo newData={data} />;
   }
 
   if (["web", "twitter", "whatsapp", "facebook"].includes(data?.qrType)) {
-    return <Web urlString={handleDesignerString(data.qrType, data)}/>;
+    return <Web urlString={handleDesignerString(data.qrType, data)} />;
   }
 
   if (ASSETS.includes(data?.qrType)) {
-    return <FileMicro newData={data}/>;
+    return <FileMicro newData={data} />;
   }
 
-  if (data?.qrType === "donations") {
-    return <Donations newData={data}/>;
+  if (data?.qrType === "donations" || "donation") {
+    return <Donations newData={data} />;
   }
 
   if (data?.qrType === "link") {
-    return <LinksMicro newData={data}/>;
+    return <LinksMicro newData={data} />;
   }
 
-  if(newData.qrType === "petId"){
-    return <PetsId newData={newData}/>
+  if (newData.qrType === "petId") {
+    return <PetsId newData={newData} />
   }
 
   return (
@@ -125,16 +125,16 @@ export default function MainComponent({newData}: MainCompProps) {
       transform: "translate(-50%, -50%)",
     }}>
       {(!data && newData.isAnEmptyPreview) ? (
-        <Typography sx={{textAlign: 'center'}}>{'Preparing preview...'}</Typography>
+        <Typography sx={{ textAlign: 'center' }}>{'Preparing preview...'}</Typography>
       ) : (
-      <Box sx={{display: "flex", flexDirection: "column", width: "100%"}}>
-        <EngineeringIcon color="primary" sx={{mx: "auto", fontSize: "50px"}}/>
-        <Typography sx={{mx: "auto"}}>{"Work in progress."}</Typography>
-        <Typography sx={{
-          color: theme => theme.palette.text.disabled,
-          mx: "auto",
-        }}>{"This is going to be ready very soon."}</Typography>
-      </Box>
+        <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+          <EngineeringIcon color="primary" sx={{ mx: "auto", fontSize: "50px" }} />
+          <Typography sx={{ mx: "auto" }}>{"Work in progress."}</Typography>
+          <Typography sx={{
+            color: theme => theme.palette.text.disabled,
+            mx: "auto",
+          }}>{"This is going to be ready very soon."}</Typography>
+        </Box>
       )}
     </Box>
   );
