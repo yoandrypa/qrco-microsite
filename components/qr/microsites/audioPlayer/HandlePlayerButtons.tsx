e9565aaeb53a4b0069f3bfed942498c997db5678
soma <soma@velosimo.com>
2023-01-23 09:49:46 -0500

    updating label

---
+++ components/qr/microsites/audioPlayer/HandlePlayerButtons.tsx
@@ -0,0 +1,34 @@
+import {memo} from 'react';
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+
+interface Props {
+  disabled: boolean;
+  playing: boolean;
+}
+
+interface HandlePlayerButtonsProps extends Props {
+  handlePlayPause: () => void;
+}
+
+const HandlePlayerButtons = ({disabled, playing, handlePlayPause}: HandlePlayerButtonsProps) => {
+  return (
+    <Box sx={{width: '100%', textAlign: 'center'}}>
+      <IconButton
+        sx={{background: 'azure', border: 'solid 1px blue', color: 'blue'}}
+        disabled={disabled}
+        onClick={handlePlayPause}
+      >
+        {playing ? <PauseIcon/> : <PlayArrowIcon/>}
+      </IconButton>
+    </Box>
+  );
+}
+
+function notIf(current: Props, next: Props) {
+  return current.disabled === next.disabled && current.playing === next.playing;
+}
+
+export default memo(HandlePlayerButtons, notIf);
---
+++ pages/player.tsx
@@ -0,0 +1,5 @@
+import AudioPlayer from "../components/qr/microsites/audioPlayer/AudioPlayer";
+
+export default function Player() {
+  return <AudioPlayer />;
+}
