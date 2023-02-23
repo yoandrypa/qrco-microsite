0709fe393074a051305da661a544a9044f5b85d2
soma <soma@velosimo.com>
2023-02-23 17:29:08 -0500

    fixing pregenerated search for

--- queries/preGenerated.ts
+++ queries/preGenerated.ts
@@ -8,18 +8,14 @@ import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 export const get = async (key: string) => {
   try {
-    const prefix: string = process.env.REACT_NODE_ENV === "production"
-      ? "prd_"
-      : "dev_";
+    const prefix: string = process.env.REACT_NODE_ENV === "production" ? "prd_" : "dev_";
     const input: ExecuteStatementCommandInput = {
-      Statement: "SELECT * FROM " + prefix +
-        "pre_generated WHERE id=?",
+      Statement: "SELECT * FROM " + prefix + "pre_generated WHERE code=?",
       Parameters: [{ "S": key }],
     };
 
     const command: ExecuteStatementCommand = new ExecuteStatementCommand(input);
-    const response: ExecuteStatementCommandOutput = await ddbClient.send(
-      command);
+    const response: ExecuteStatementCommandOutput = await ddbClient.send(command);
     // @ts-ignore
     return response.Items[0] ? unmarshall(response.Items[0]) : null;
   } catch (e) {
