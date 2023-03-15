6d3ee473f8fae6c793228e0dfec0a3c3948fd683
yoandrypa <yoandrypa@gmail.com>
2023-03-15 09:52:31 -0400

    Add index to contact form service

--- pages/api/send/contact/email/helpers.ts
+++ pages/api/send/contact/email/helpers.ts
@@ -11,6 +11,7 @@ export { NotFound, respondWithException } from "../../../../../libs/exceptions";
  */
 export async function parseFromPostRequest(req: NextApiRequest): Promise<any> {
   const schema = Joi.object({
+    index: Joi.number().min(0).optional(),
     contactEmail: Joi.string().required().regex(/^\w+([.+]\w+)?@(\w+\.)+([a-z]{2,3})/),
     subject: Joi.string().required(),
     content: Joi.object({
@@ -19,12 +20,12 @@ export async function parseFromPostRequest(req: NextApiRequest): Promise<any> {
     }).required(),
   });
 
-  const { contactEmail, ...data } = Joi.attempt(req.body, schema, { abortEarly: false });
+  const { index, contactEmail, ...data } = Joi.attempt(req.body, schema, { abortEarly: false });
 
   return {
     ...data,
     fromAddresses: 'info@ebanux.com',
-    toAddresses: await getEmailRecipient(data.content.microSiteUrl),
+    toAddresses: await getEmailRecipient(data.content.microSiteUrl, index),
     replyToAddresses: contactEmail,
     template: 'contact-form-message',
   }
@@ -34,11 +35,11 @@ function parseCodeFormUrl(url: string): string {
   return url.split('/').pop() as string
 }
 
-async function getEmailRecipient(microSiteUrl: string): Promise<string> {
+async function getEmailRecipient(microSiteUrl: string, index?: number): Promise<string> {
   const code = parseCodeFormUrl(microSiteUrl);
   const link = await queries.link.getByAddress(code);
   const qr: any = await queries.qr.getByLinkId(link);
-  const contact: any = qr.custom.find(({ component }: any) => component === 'contact');
+  const contact: any = index !== undefined ? qr.custom[index] : qr.custom.find(({ component }: any) => component === 'contact');
   const { email } = contact.data;
 
   return email;
