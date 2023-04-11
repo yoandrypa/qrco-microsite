3bae2eca2d5ee4e815160bddc97f9120da9338fe
soma <soma@velosimo.com>
2023-04-11 13:59:34 -0400

    updating showqr page

--- components/qr/microsites/mainMicroComponents/RenderShowQr.tsx
+++ components/qr/microsites/mainMicroComponents/RenderShowQr.tsx
@@ -3,13 +3,18 @@ import DownloadIcon from "@mui/icons-material/Download";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import {memo} from 'react';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import {memo, useEffect, useState} from 'react';
+import {handleButtons, handleFont} from "../renderers/helper";
+import {useTheme} from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Notifications from "../../helperComponents/Notifications";
 
 interface RenderQrProps {
   handlePreviewQr: () => void;
   qrImg: any;
-  height: string;
-  iframed: boolean;
+  data: any;
 }
 
 const downloadAsSVG = async (imageSrc: any) => {
@@ -32,32 +37,74 @@ const downloadAsSVG = async (imageSrc: any) => {
   element.remove();
 };
 
-function RenderShowQr({handlePreviewQr, qrImg, height, iframed}: RenderQrProps) {
+function RenderShowQr({handlePreviewQr, qrImg, data}: RenderQrProps) {
+  const [url, setURL] = useState<string>('');
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const theme = useTheme();
+
+  useEffect(() => {
+    if (window) {
+      setURL(window.location.href);
+    }
+  }, []);
 
   // noinspection JSDeprecatedSymbols
   return (
     <Box sx={{width: '100%', p: 2, background: '#fff'}}>
       <Box sx={{textAlign: 'center'}}>
-        <Box component="img" src="/logo.svg" sx={{width: '70%', mb: '25px'}} />
+        <Box component="img" src="/logo.svg" sx={{width: '130px', mt: 2, mb: '10px', py: '5px', px: 3}} />
+        {url.length > 0 && (
+          <Box sx={{display: 'flex', width: '100%', borderRadius: '4px 4px 0 0', pt: 1, justifyContent: 'center'}}>
+            <Typography sx={{my: 'auto'}}>{url}</Typography>
+            <Tooltip title="Copy URL">
+              <IconButton onClick={() => {
+                try {
+                navigator.clipboard.writeText(url);
+                setCopied(true);
+              } catch {
+                console.log('Copy failed');
+              }
+              }}><ContentCopyIcon /></IconButton>
+            </Tooltip>
+          </Box>
+        )}
         <Box
           component="img"
           alt="qrLynkImage"
           src={qrImg.content || `data:image/svg+xml;base64,${btoa(qrImg)}`}
           sx={{
-            border: 'solid 1px #d7d7d7',
+            border: 'solid 1px #ccc',
             borderRadius: '4px',
             position: 'relative'
           }}
         />
         <Box sx={{width: '100%', display: 'flex', justifyContent: 'space-between', mt: 2}}>
-          <Button variant="outlined" startIcon={<ArrowBackIcon />} onClick={handlePreviewQr}>Back</Button>
-          <Button variant="outlined" startIcon={<DownloadIcon />} onClick={async () => downloadAsSVG(qrImg)}>Download as SVG</Button>
+          <Button
+            variant="outlined" startIcon={<ArrowBackIcon />} onClick={handlePreviewQr}
+            sx={{...handleFont(data, 'd'), ...handleButtons(data, theme)}}>
+            {'Back'}
+          </Button>
+          <Button
+            variant="outlined" startIcon={<DownloadIcon />} onClick={async () => downloadAsSVG(qrImg)}
+            sx={{...handleFont(data, 'd'), ...handleButtons(data, theme)}}>
+            {'Download as SVG'}
+          </Button>
         </Box>
       </Box>
-      {iframed && (
+      {(data?.iframed || false) && (
         <Typography sx={{bottom: '40px', position: 'fixed', textAlign: 'center', color: theme => theme.palette.text.disabled}}>
           {'You are seeing the embedded version of this app. Some widgets might differ from their final functionality. Be advised.'}
         </Typography>
+      )}
+      {copied && (
+        <Notifications
+          autoHideDuration={2500}
+          message="Copied!"
+          vertical="bottom"
+          horizontal="center"
+          severity="success"
+          onClose={() => setCopied(false)}/>
       )}
     </Box>
   );
