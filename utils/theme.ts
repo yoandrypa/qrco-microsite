66ac8c36f7249688cd20070c619308b01d4bbd0e
yoandrypa <yoandrypa@gmail.com>
2023-05-04 12:37:02 -0400

    Fix form-field styles and theme

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,4 +1,10 @@
+import { Theme } from "@mui/material/styles";
 import { THEMES_VARIANTS, PRIMARY_LIGHT_COLOR, PRIMARY_DARK_COLOR, HEADER_HEIGHT } from "../consts";
+
+interface IBaseProps {
+  ownerState: any;
+  theme: Theme;
+}
 
 export const themeConfig = (mode = THEMES_VARIANTS.light) => {
   const primaryColor = mode === THEMES_VARIANTS.light ? PRIMARY_LIGHT_COLOR : PRIMARY_DARK_COLOR;
@@ -17,7 +23,27 @@ export const themeConfig = (mode = THEMES_VARIANTS.light) => {
             minHeight: `${HEADER_HEIGHT}px !important`
           }
         }
-      }
+      },
+
+      MuiFormLabel: {
+        styleOverrides: {
+          root: ({ theme }: IBaseProps) => {
+            return {
+              paddingLeft: theme.spacing(1),
+              paddingRight: theme.spacing(1),
+              marginLeft: theme.spacing(-0.5),
+              borderRadius: theme.spacing(1),
+              backgroundColor: theme.palette.background.default,
+              '&.Mui-focused': {
+                borderColor: theme.palette.primary.main,
+                borderWidth: 1,
+                borderStyle: 'solid',
+              }
+            }
+          }
+        }
+      },
+
     }
   };
 };
