7f8c373c608abaa0695506633612e88a2f4d32fe
soma <soma@velosimo.com>
2023-05-09 17:19:50 -0400

    loading external data for buttons

--- queries/storage.ts
+++ queries/storage.ts
@@ -5,13 +5,8 @@ import {
 
 export const download = async (key: string, isSample?: boolean) => {
   try {
-    const bucket: string = !isSample
-      ? String(process.env.REACT_AWS_BUCKET_NAME)
-      : String(process.env.REACT_AWS_SAMPLE_BUCKET_NAME);
-    const downloadParams: GetObjectCommandInput = {
-      Key: key,
-      Bucket: bucket
-    };
+    const bucket = (!isSample ? process.env.REACT_AWS_BUCKET_NAME : process.env.REACT_AWS_SAMPLE_BUCKET_NAME) as string;
+    const downloadParams: GetObjectCommandInput = { Key: key, Bucket: bucket };
     return await s3Client.getObject(downloadParams);
   } catch (e) {
     throw e;
